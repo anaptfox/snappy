0a1d9e8eee4143098cb485e5532886c63ac36e84
Taron Foxworth <taronfoxworth@gmail.com>
2017-06-03 15:43:36 -0400

    twitter

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const NodeWebcam = require('node-webcam');
 const Dropbox = require('dropbox');
 const fs = require('fs');
 const uuid = require('uuid/v4');
+const Twitter = require('twitter');
 
-const DROPBOX_ACCESS_TOKEN = 'DROPBOX_ACCESS_TOKEN';
-const LOSANT_DEVICE_ID = 'LOSANT_DEVICE_ID';
-const LOSANT_DEVICE_KEY = 'LOSANT_DEVICE_KEY';
-const LOSANT_DEVICE_SECRET = 'LOSANT_DEVICE_SECRET';
-const WEBCAM_ONE = 'HUE HD Camera';
-const WEBCAM_TWO = 'HUE HD Camera';
+const DROPBOX_ACCESS_TOKEN = '';
+const LOSANT_DEVICE_ID = '';
+const LOSANT_DEVICE_KEY = '';
+const LOSANT_DEVICE_SECRET = '';
+const WEBCAM_ONE = '/dev/video0';
+const WEBCAM_TWO = '/dev/video1';
+var client = new Twitter({consumer_key: '', consumer_secret: '', access_token_key: '', access_token_secret: ''});
 
 var WebcamOne = NodeWebcam.create({device: WEBCAM_ONE});
-
 var WebcamTwo = NodeWebcam.create({device: WEBCAM_TWO});
-
 var Device = require('losant-mqtt').Device;
 
 // Construct device.
@@ -58,6 +58,30 @@ function uploadFile(file) {
     contents: fileContents
   }).then(function(response) {
     console.log('File uploaded!');
+    // Make post request on media endpoint. Pass file data as media parameter
+    client.post('media/upload', {
+      media: fileContents
+    }, function(error, media, response) {
+
+      if (!error) {
+
+        // If successful, a media object will be returned.
+        console.log(media);
+
+        // Lets tweet it
+        var status = {
+          status: 'Hanging out with LeBron through a VR experience in the HPE Pointnext Venues demo',
+          media_ids: media.media_id_string // Pass the media id string
+        }
+
+        client.post('statuses/update', status, function(error, tweet, response) {
+          if (!error) {
+            console.log(tweet);
+          }
+        });
+
+      }
+    });
     //console.log(response);
   }).catch(function(error) {
     console.log('File FAILED to upload:');
